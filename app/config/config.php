c2cab6ec98a8845a63e3b4abecc37be7b97ffefa
munozdaniel <munozda87@hotmail.com>
2015-08-25 13:40:22 -0300

    Configuracion para la BD

--- app/config/config.php
+++ app/config/config.php
@@ -5,10 +5,10 @@ defined('APP_PATH') || define('APP_PATH', realpath('.'));
 return new \Phalcon\Config(array(
     'database' => array(
         'adapter'     => 'Mysql',
-        'host'        => 'localhost',
+        'host'        => '192.168.42.14',
         'username'    => 'root',
-        'password'    => '',
-        'dbname'      => 'test',
+        'password'    => 'infoimps',
+        'dbname'      => 'encuestaMelewe',
         'charset'     => 'utf8',
     ),
     'application' => array(
