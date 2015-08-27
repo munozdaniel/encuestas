674061eecf70233f1c9dce3c6ec59f6ccaa39719
munozdaniel <munozda87@hotmail.com>
2015-08-27 08:33:08 -0300

    Agregado cache de index.volt y main.volt

--- app/cache/c__xampp_htdocs_encuestas_app_views_layouts_main.volt.php
+++ app/cache/c__xampp_htdocs_encuestas_app_views_layouts_main.volt.php
@@ -13,164 +13,15 @@
                 </button>
                 <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt="logo"></a>
             </div>
-
             <div class="collapse navbar-collapse navbar-right">
                 <ul class="nav navbar-nav">
-                    <li class="scroll active"><a href="#home">Inicio</a></li>
-                    <li class="scroll"><a href="#features">Encuestas</a></li>
-                    <li class="scroll"><a href="#get-in-touch">Contactos</a></li>
-                    <li class=""><a href="#get-in-touch">Login</a></li>
+                    <?php echo $this->elemento->getMenu(); ?>
                 </ul>
             </div>
         </div><!--/.container-->
     </nav><!--/nav-->
 </header><!--/header-->
-
-<section id="main-slider">
-    <div class="owl-carousel">
-        <div class="item" style="background-image: url(images/slider/bg1.jpg);">
-            <div class="slider-inner">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="carousel-content">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--/.item-->
-        <div class="item" style="background-image: url(images/slider/bg2.jpg);">
-            <div class="slider-inner">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="carousel-content">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--/.item-->
-        <div class="item" style="background-image: url(images/slider/bg2.jpg);">
-            <div class="slider-inner">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-sm-6">
-                            <div class="carousel-content">
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div><!--/.item-->
-    </div><!--/.owl-carousel-->
-</section><!--/#main-slider-->
-
-
-<section id="features">
-    <div class="container">
-        <div class="section-header">
-            <h2 class="section-title text-center wow fadeInDown">Awesome Features</h2>
-            <p class="text-center wow fadeInDown">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
-        </div>
-        <div class="row">
-            <div class="col-sm-6 wow fadeInLeft">
-                <img class="img-responsive" src="images/main-feature.png" alt="">
-            </div>
-            <div class="col-sm-6">
-                <div class="media service-box wow fadeInRight">
-                    <div class="pull-left">
-                        <i class="fa fa-line-chart"></i>
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">UX design</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
-                    </div>
-                </div>
-
-                <div class="media service-box wow fadeInRight">
-                    <div class="pull-left">
-                        <i class="fa fa-cubes"></i>
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">UI design</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
-                    </div>
-                </div>
-
-                <div class="media service-box wow fadeInRight">
-                    <div class="pull-left">
-                        <i class="fa fa-pie-chart"></i>
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">SEO Services</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
-                    </div>
-                </div>
-
-                <div class="media service-box wow fadeInRight">
-                    <div class="pull-left">
-                        <i class="fa fa-pie-chart"></i>
-                    </div>
-                    <div class="media-body">
-                        <h4 class="media-heading">SEO Services</h4>
-                        <p>Backed by some of the biggest names in the industry, Firefox OS is an open platform that fosters greater</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-<section id="get-in-touch">
-    <div class="container">
-        <div class="section-header">
-            <h2 class="section-title text-center wow fadeInDown">Get in Touch</h2>
-            <p class="text-center wow fadeInDown">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut <br> et dolore magna aliqua. Ut enim ad minim veniam</p>
-        </div>
-    </div>
-</section><!--/#get-in-touch-->
-
-
-<section id="contact">
-    <div id="google-map" style="height:650px" data-latitude="52.365629" data-longitude="4.871331"></div>
-    <div class="container-wrapper">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-4 col-sm-offset-8">
-                    <div class="contact-form">
-                        <h3>Contact Info</h3>
-
-                        <address>
-                            <strong>Twitter, Inc.</strong><br>
-                            795 Folsom Ave, Suite 600<br>
-                            San Francisco, CA 94107<br>
-                            <abbr title="Phone">P:</abbr> (123) 456-7890
-                        </address>
-
-                        <form id="main-contact-form" name="contact-form" method="post" action="#">
-                            <div class="form-group">
-                                <input type="text" name="name" class="form-control" placeholder="Name" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="email" name="email" class="form-control" placeholder="Email" required>
-                            </div>
-                            <div class="form-group">
-                                <input type="text" name="subject" class="form-control" placeholder="Subject" required>
-                            </div>
-                            <div class="form-group">
-                                <textarea name="message" class="form-control" rows="8" placeholder="Message" required></textarea>
-                            </div>
-                            <button type="submit" class="btn btn-primary">Send Message</button>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section><!--/#bottom-->
+<?php echo $this->getContent(); ?>
 <footer id="footer">
     <div class="container">
         <div class="row">
@@ -180,15 +31,6 @@
             <div class="col-sm-6">
                 <ul class="social-icons">
                     <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                    <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                    <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                    <li><a href="#"><i class="fa fa-pinterest"></i></a></li>
-                    <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
-                    <li><a href="#"><i class="fa fa-behance"></i></a></li>
-                    <li><a href="#"><i class="fa fa-flickr"></i></a></li>
-                    <li><a href="#"><i class="fa fa-youtube"></i></a></li>
-                    <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
-                    <li><a href="#"><i class="fa fa-github"></i></a></li>
                 </ul>
             </div>
         </div>
