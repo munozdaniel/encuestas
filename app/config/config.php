4f2246efa5b4976a4e165470b40b98995cac342e
munozdaniel <munozda87@hotmail.com>
2015-08-31 11:33:15 -0300

    loaders: Agregado directorio forms.

--- app/config/config.php
+++ app/config/config.php
@@ -27,6 +27,7 @@ return new \Phalcon\Config(array(
         'pluginsDir'     => APP_PATH . '/app/plugins/',
         'libraryDir'     => APP_PATH . '/app/library/',
         'cacheDir'       => APP_PATH . '/app/cache/',
+        'forms'       => APP_PATH . '/app/forms/',
         'baseUri'        => '/encuestas/',
     )
 ));
