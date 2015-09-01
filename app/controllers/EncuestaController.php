5cfd039c774fb6ad9c9299bbc017339aacc6ed66
munozdaniel <munozda87@hotmail.com>
2015-09-01 14:22:58 -0300

    EncuestaController: Enviando formularios a la vista

--- app/controllers/EncuestaController.php
+++ app/controllers/EncuestaController.php
@@ -1,12 +1,29 @@
 <?php
 
-class EncuestaController extends \Phalcon\Mvc\Controller
+class EncuestaController extends ControllerBase
 {
+    public function initialize()
+    {
+        $this->tag->setTitle('Bienvenidos');
+        parent::initialize();
 
+    }
     public function indexAction()
     {
 
     }
+
+    /**
+     * Villa La Angostura: Muestra la vista para ingresar los datos de la encuesta con
+     * componentes elaborados en VillaEncuestaForm.
+     */
+    public function villaAction()
+    {
+        $this->view->encuestaForm = new VillaEncuestaForm;
+        $this->view->unidadForm = new VillaUnidadForm();
+        $this->view->recepcionForm = new VillaRecepcionForm();
+    }
+
 
 }
 
