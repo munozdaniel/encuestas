a85d4f41570eb3a5ae71a54c9ed55244c5114dd3
munozdaniel <munozda87@hotmail.com>
2015-09-14 12:18:10 -0300

    Modelo tipoPax

---
+++ app/models/Tipopax.php
@@ -0,0 +1,58 @@
+<?php
+
+class Tipopax extends \Phalcon\Mvc\Model
+{
+
+    /**
+     *
+     * @var integer
+     */
+    public $tipoPax_id;
+
+    /**
+     *
+     * @var string
+     */
+    public $tipoPax_nombre;
+
+    /**
+     * Initialize method for model.
+     */
+    public function initialize()
+    {
+        $this->setSource("'tipoPax'");
+    }
+
+    /**
+     * Returns table name mapped in the model.
+     *
+     * @return string
+     */
+    public function getSource()
+    {
+        return 'tipoPax';
+    }
+
+    /**
+     * Allows to query a set of records that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Tipopax[]
+     */
+    public static function find($parameters = null)
+    {
+        return parent::find($parameters);
+    }
+
+    /**
+     * Allows to query the first record that match the specified conditions
+     *
+     * @param mixed $parameters
+     * @return Tipopax
+     */
+    public static function findFirst($parameters = null)
+    {
+        return parent::findFirst($parameters);
+    }
+
+}
