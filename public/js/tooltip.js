b36e98ec46bb4b187781c537aa9e03a4ed5891de
munozdaniel <munozda87@hotmail.com>
2015-09-16 11:26:17 -0300

    Tooltip.js

---
+++ public/js/tooltip.js
@@ -0,0 +1,13 @@
+/**
+ * Created by dmunioz on 16/09/2015.
+ */
+jQuery(function($) {
+
+    //Slider SI LO USO
+    $(document).ready(function() {
+        $('[data-toggle="tooltip"]').tooltip({
+            container: 'body'
+        });
+
+    });
+});
