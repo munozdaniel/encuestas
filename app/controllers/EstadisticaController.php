acf3aea7a64cab8f0c17a0286e3e8e7fe78fc4fd
Munoz Daniel <munozda87@hotmail.com>
2015-09-20 12:14:58 -0300

    Nuevo Controlador Estadistica

---
+++ app/controllers/EstadisticaController.php
@@ -0,0 +1,17 @@
+<?php
+
+class EstadisticaController extends ControllerBase
+{
+    public function initialize()
+    {
+        $this->tag->setTitle('Estadisticas');
+        parent::initialize();
+    }
+
+    public function indexAction()
+    {
+
+    }
+
+}
+
