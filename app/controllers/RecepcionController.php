f090b83d49347fa40e6847f248ecfbfd13ce6bfc
munozdaniel <munozda87@hotmail.com>
2016-01-25 08:56:29 -0300

    RecepcionController: Guardando una recepcion.

---
+++ app/controllers/RecepcionController.php
@@ -0,0 +1,235 @@
+<?php
+ 
+use Phalcon\Mvc\Model\Criteria;
+use Phalcon\Paginator\Adapter\Model as Paginator;
+
+class RecepcionController extends ControllerBase
+{
+
+    /**
+     * Etapa Nº2
+     */
+    public function indexAction()
+    {
+    //        $this->persistent->parameters = null;
+    }
+
+    /**
+     * Searches for recepcion
+     */
+    public function searchAction()
+    {
+
+        $numberPage = 1;
+        if ($this->request->isPost()) {
+            $query = Criteria::fromInput($this->di, "Recepcion", $_POST);
+            $this->persistent->parameters = $query->getParams();
+        } else {
+            $numberPage = $this->request->getQuery("page", "int");
+        }
+
+        $parameters = $this->persistent->parameters;
+        if (!is_array($parameters)) {
+            $parameters = array();
+        }
+        $parameters["order"] = "recepcion_id";
+
+        $recepcion = Recepcion::find($parameters);
+        if (count($recepcion) == 0) {
+            $this->flash->notice("The search did not find any recepcion");
+
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "index"
+            ));
+        }
+
+        $paginator = new Paginator(array(
+            "data" => $recepcion,
+            "limit"=> 10,
+            "page" => $numberPage
+        ));
+
+        $this->view->page = $paginator->getPaginate();
+    }
+
+    /**
+     * Displays the creation form
+     */
+    public function newAction()
+    {
+        $this->view->recepcionForm = new RecepcionForm();
+
+    }
+
+    /**
+     * Edits a recepcion
+     *
+     * @param string $recepcion_id
+     */
+    public function editAction($recepcion_id)
+    {
+
+        if (!$this->request->isPost()) {
+
+            $recepcion = Recepcion::findFirstByrecepcion_id($recepcion_id);
+            if (!$recepcion) {
+                $this->flash->error("recepcion was not found");
+
+                return $this->dispatcher->forward(array(
+                    "controller" => "recepcion",
+                    "action" => "index"
+                ));
+            }
+
+            $this->view->recepcion_id = $recepcion->recepcion_id;
+
+            $this->tag->setDefault("recepcion_id", $recepcion->getRecepcionId());
+            $this->tag->setDefault("recepcion_puntajeNivelId", $recepcion->getRecepcionPuntajenivelid());
+            $this->tag->setDefault("recepcion_puntajeTiempoId", $recepcion->getRecepcionPuntajetiempoid());
+            $this->tag->setDefault("recepcion_puntajeTratoId", $recepcion->getRecepcionPuntajetratoid());
+            $this->tag->setDefault("recepcion_tieneInconvenientes", $recepcion->getRecepcionTieneinconvenientes());
+            $this->tag->setDefault("recepcion_comentario", $recepcion->getRecepcionComentario());
+            $this->tag->setDefault("recepcion_habilitado", $recepcion->getRecepcionHabilitado());
+            
+        }
+    }
+
+    /**
+     * Creates a new recepcion
+     */
+    public function createAction()
+    {
+
+        if (!$this->request->isPost()) {
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "index"
+            ));
+        }
+
+        $recepcion = new Recepcion();
+
+        $recepcion->setRecepcionPuntajenivelid($this->request->getPost("recepcion_puntajeNivelId"));
+        $recepcion->setRecepcionPuntajetiempoid($this->request->getPost("recepcion_puntajeTiempoId"));
+        $recepcion->setRecepcionPuntajetratoid($this->request->getPost("recepcion_puntajeTratoId"));
+        $recepcion->setRecepcionTieneinconvenientes($this->request->getPost("recepcion_puntajeInconvenientes")-1);//Le resto 1 porque el RadioGroup Genera los values a partir de 1
+        $recepcion->setRecepcionComentario($this->request->getPost("recepcion_comentario"));
+        $recepcion->setRecepcionHabilitado(1);
+
+
+        if (!$recepcion->save()) {
+            foreach ($recepcion->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "new"
+            ));
+        }
+
+        $this->flash->success("PASO Nº 2 COMPLETADO CON EXITO! <i class='fa fa-thumbs-up'></i>");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "unidad",
+            "action" => "new"
+        ));
+
+    }
+
+    /**
+     * Saves a recepcion edited
+     *
+     */
+    public function saveAction()
+    {
+
+        if (!$this->request->isPost()) {
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "index"
+            ));
+        }
+
+        $recepcion_id = $this->request->getPost("recepcion_id");
+
+        $recepcion = Recepcion::findFirstByrecepcion_id($recepcion_id);
+        if (!$recepcion) {
+            $this->flash->error("recepcion does not exist " . $recepcion_id);
+
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "index"
+            ));
+        }
+
+        $recepcion->setRecepcionPuntajenivelid($this->request->getPost("recepcion_puntajeNivelId"));
+        $recepcion->setRecepcionPuntajetiempoid($this->request->getPost("recepcion_puntajeTiempoId"));
+        $recepcion->setRecepcionPuntajetratoid($this->request->getPost("recepcion_puntajeTratoId"));
+        $recepcion->setRecepcionTieneinconvenientes($this->request->getPost("recepcion_tieneInconvenientes"));
+        $recepcion->setRecepcionComentario($this->request->getPost("recepcion_comentario"));
+        $recepcion->setRecepcionHabilitado($this->request->getPost("recepcion_habilitado"));
+        
+
+        if (!$recepcion->save()) {
+
+            foreach ($recepcion->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "edit",
+                "params" => array($recepcion->recepcion_id)
+            ));
+        }
+
+        $this->flash->success("recepcion was updated successfully");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "recepcion",
+            "action" => "index"
+        ));
+
+    }
+
+    /**
+     * Deletes a recepcion
+     *
+     * @param string $recepcion_id
+     */
+    public function deleteAction($recepcion_id)
+    {
+
+        $recepcion = Recepcion::findFirstByrecepcion_id($recepcion_id);
+        if (!$recepcion) {
+            $this->flash->error("recepcion was not found");
+
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "index"
+            ));
+        }
+
+        if (!$recepcion->delete()) {
+
+            foreach ($recepcion->getMessages() as $message) {
+                $this->flash->error($message);
+            }
+
+            return $this->dispatcher->forward(array(
+                "controller" => "recepcion",
+                "action" => "search"
+            ));
+        }
+
+        $this->flash->success("recepcion was deleted successfully");
+
+        return $this->dispatcher->forward(array(
+            "controller" => "recepcion",
+            "action" => "index"
+        ));
+    }
+
+}
